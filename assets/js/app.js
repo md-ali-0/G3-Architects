ee413270bd52f1a04974bedc1f28b7410303fae3
md-ali-0 <mohammad.98482@gmail.com>
2023-07-18 09:28:10 +0600

    G3-Architects Header Section Added

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+const navTogger = document.querySelector(".nav-toggle");
+navTogger.addEventListener("click", navToggo);
+
+function navToggo(){
+    //  navTogger.classList.toggle("active");
+     const nav = document.querySelector("nav");
+     nav.classList.toggle("open");
+     if(nav.classList.contains("open") ){
+         nav.style.maxHeight = nav.scrollHeight + "px";
+     }else{
+         nav.removeAttribute("style")
+     }
+    ;
+}
